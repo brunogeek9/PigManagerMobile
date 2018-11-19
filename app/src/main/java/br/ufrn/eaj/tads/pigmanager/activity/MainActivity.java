02011b30d989291a84f38fff7d1171cf8be40e6d
null <null>
2018-11-19 14:14:06 -0300

    Adicionado intereções de Login e LogOut.

--- app/src/main/java/br/ufrn/eaj/tads/pigmanager/activity/MainActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/pigmanager/activity/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 import br.ufrn.eaj.tads.pigmanager.R;
 import br.ufrn.eaj.tads.pigmanager.fragments.ConfiguracaoFragment;
@@ -20,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar tool = findViewById(R.id.toolbar);
+        tool.setTitle("PigManager");
+        setSupportActionBar(tool);
 
         BottomNavigationView bottomnav = findViewById(R.id.bottom_navigation);
         //Desativar animação
@@ -59,5 +65,29 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sair, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //AuthUI.getInstance().signOut(this);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
